afa7cf4add3b9877d4c8f54ee435855da8e88173
tanotu0036 <tanotu@kodomohoikuen.com>
2026-06-09 10:38:42 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
 //  GitHub Pages のフロントからこのファイルを読み込んで使う
 // ============================================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbxXgsF-WpNvqmKIuF24eaGPVdVby1K22Gne2caEIf2aNjAmSrC1jXyD4g-A3bTQdOLz/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzckaqaCZIG6C4IJiDnUCT0F8Swy8i7j_d49ro2eJLQHeMtH0EP2vm1bumABz4Y_k1a/exec';
 
 async function gasRequest(action, params = {}, body = null) {
   const url = new URL(API_URL);
